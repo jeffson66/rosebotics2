7c7ae995c15213b3a1f0373f6ab38302e58b8a94
jeffson66 <linj3@rose-hulman.edu>
2018-11-13 23:07:39 -0500

    dude,comment out calibrate

--- src/capstone_1_runs_on_laptop.py
+++ src/capstone_1_runs_on_laptop.py
@@ -8,7 +8,7 @@ It uses MQTT to SEND information to a program running on the ROBOT.
 Authors:  David Mutchler, his colleagues, and Jiafan Lin.
 """
 # ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
+# TODO: 1.s PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
